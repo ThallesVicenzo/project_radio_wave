371e1a3f14911cde4f6743b3a581679afabafda6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-05-30 18:16:36 -0300

    feat: registers onboarding integration

--- lib/app_injector.dart
+++ lib/app_injector.dart
@@ -1,5 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:radio_wave/features/welcome/domain/repository/onboarding_repository.dart';
+import 'package:radio_wave/features/welcome/domain/usecase/onboarding_usecase.dart';
+import 'package:radio_wave/features/welcome/external/onboarding_data_source_impl.dart';
+import 'package:radio_wave/features/welcome/infra/datasource/onboarding_data_source.dart';
+import 'package:radio_wave/features/welcome/infra/repository/onboarding_repository_impl.dart';
 import 'package:radio_wave/l10n/global_app_localizations.dart';
 
 import 'core/client/client_http.dart';
@@ -15,4 +20,16 @@ Future<void> init() async {
   sl.registerSingleton<ClientHttp>(DioClient());
 
   sl.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+
+  sl.registerFactory<OnboardingDataSource>(
+    () => OnboardingDataSourceImpl(sl()),
+  );
+
+  sl.registerFactory<OnboardingRepository>(
+    () => OnboardingRepositoryImpl(sl()),
+  );
+
+  sl.registerFactory<OnboardingUsecase>(
+    () => OnboardingUsecaseImpl(sl()),
+  );
 }
