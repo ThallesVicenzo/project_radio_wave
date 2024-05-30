091fdd3a398ac41c1510a24009cb5a270581fa2e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-05-30 18:16:20 -0300

    feat: adds onboarding states

--- lib/features/welcome/presenter/cubit/onboarding_state.dart
+++ lib/features/welcome/presenter/cubit/onboarding_state.dart
@@ -1,3 +1,28 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:radio_wave/core/errors/failure.dart';
 
-abstract class OnboardingState extends Equatable {}
+abstract class OnboardingState extends Equatable {
+  const OnboardingState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OnboardingInitialState extends OnboardingState {
+  const OnboardingInitialState();
+}
+
+class OnboardingLoadingState extends OnboardingState {
+  const OnboardingLoadingState();
+}
+
+class OnboardingSuccessState extends OnboardingState {
+  final UserCredential userCredential;
+  const OnboardingSuccessState(this.userCredential);
+}
+
+class OnboardingErrorState extends OnboardingState {
+  final Failure failure;
+  const OnboardingErrorState(this.failure);
+}
