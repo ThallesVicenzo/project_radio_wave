1d5c9ff274070dbb687b358f2ab14e3368aca3f3
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-06-10 15:45:17 -0300

    feat: adds welcome cubit

---
+++ lib/features/welcome/presenter/cubit/welcome_cubit.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:radio_wave/core/presenter/routes/app_navigator.dart';
+import 'package:radio_wave/core/presenter/routes/main_routes.dart';
+import 'package:radio_wave/core/secure_storage/keys/secure_storage_keys.dart';
+import 'package:radio_wave/core/secure_storage/secure_storage.dart';
+import 'package:radio_wave/core/utils/base_cubit.dart';
+import 'package:radio_wave/features/welcome/presenter/cubit/welcome_state.dart';
+import 'package:radio_wave/features/welcome/welcome_routes.dart';
+
+class WelcomeCubit extends BaseCubit<WelcomeState> {
+  WelcomeCubit(this._secureStorage)
+      : super(
+          const WelcomeInitialState(),
+        );
+
+  final SecureStorage _secureStorage;
+
+  Future<void> redirectToCorrectPage(BuildContext context) async {
+    final bool result = json.decode(
+      await _secureStorage.read(
+              key: SecureStorageKeys.redirectOnboarding.key) ??
+          'false',
+    );
+
+    if (result == true) {
+      AppNavigator(context).goNamed(
+        MainRoutes.home.route,
+      );
+    } else {
+      AppNavigator(context).pushNamed(
+        WelcomeRoutes.onboardingPage.route,
+      );
+    }
+  }
+}
