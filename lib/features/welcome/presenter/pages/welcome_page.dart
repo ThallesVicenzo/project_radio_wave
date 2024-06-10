89f33ca91c6fcd81559d8ec27c7032ed72fee845
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-06-10 15:45:50 -0300

    feat: adds redirection to correct page on welcome

--- lib/features/welcome/presenter/pages/welcome_page.dart
+++ lib/features/welcome/presenter/pages/welcome_page.dart
@@ -7,9 +7,9 @@ import 'package:design_system/widgets/colors/ui_overlay_color.dart';
 import 'package:design_system/widgets/custom_padding/custom_padding_page.dart';
 import 'package:design_system/widgets/image/asset_image_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:radio_wave/app_injector.dart';
-import 'package:radio_wave/core/presenter/routes/app_navigator.dart';
-import 'package:radio_wave/features/welcome/welcome_routes.dart';
+import 'package:radio_wave/features/welcome/presenter/cubit/welcome_cubit.dart';
 import 'package:radio_wave/l10n/global_app_localizations.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -39,41 +39,46 @@ class _WelcomePageState extends State<WelcomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return UiOverlayColor(
-      overlayColor: Colors.transparent,
-      child: Scaffold(
-        body: Stack(
-          children: [
-            const SizedBox.expand(
-              child: AssetImageWidget(
-                image: AppImage.backgroundImage,
-                fit: BoxFit.fill,
+    return BlocProvider(
+      create: (context) => WelcomeCubit(sl()),
+      child: UiOverlayColor(
+        overlayColor: Colors.transparent,
+        child: Scaffold(
+          body: Stack(
+            children: [
+              const SizedBox.expand(
+                child: AssetImageWidget(
+                  image: AppImage.backgroundImage,
+                  fit: BoxFit.fill,
+                ),
               ),
-            ),
-            CustomPaddingPage(
-              topPadding: MediaQuery.of(context).size.height * 0.1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Logo(),
-                  const SizedBox(height: 100),
-                  AnimatedOpacity(
-                    opacity: opacity,
-                    duration: kThemeAnimationDuration,
-                    child: DefaultButton(
-                      title: intl.welcomeButtonTitle,
-                      size: const Size(double.infinity, 56),
-                      onPressed: () {
-                        AppNavigator(context).pushNamed(
-                          WelcomeRoutes.onboardingPage.route,
-                        );
-                      },
-                    ),
-                  ),
-                ],
+              CustomPaddingPage(
+                topPadding: MediaQuery.of(context).size.height * 0.1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Logo(),
+                    const SizedBox(height: 100),
+                    Builder(builder: (context) {
+                      return AnimatedOpacity(
+                        opacity: opacity,
+                        duration: kThemeAnimationDuration,
+                        child: DefaultButton(
+                          title: intl.welcomeButtonTitle,
+                          size: const Size(double.infinity, 56),
+                          onPressed: () async {
+                            await context
+                                .read<WelcomeCubit>()
+                                .redirectToCorrectPage(context);
+                          },
+                        ),
+                      );
+                    }),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
