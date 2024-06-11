f828cbcd701fef157e346d336b8b2ae13825b300
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-06-11 16:07:14 -0300

    feat: adds radio entity

---
+++ lib/features/home/domain/radio_entity.dart
@@ -0,0 +1,11 @@
+class RadioEntity {
+  final String name;
+  final String url;
+  final String image;
+
+  RadioEntity({
+    required this.name,
+    required this.url,
+    required this.image,
+  });
+}
