d52ca76012555d75c6e2333bd6cd14be718ccecf
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-06-11 15:29:12 -0300

    feat: adds radio widget

---
+++ lib/features/home/presenter/widgets/radio_widget.dart
@@ -0,0 +1,36 @@
+import 'package:design_system/widgets/image/network_image_widget.dart';
+import 'package:design_system/widgets/text/app_text.dart';
+import 'package:flutter/material.dart';
+
+class RadioWidget extends StatelessWidget {
+  const RadioWidget({
+    super.key,
+    required this.radioName,
+    required this.url,
+  });
+
+  final String radioName;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          NetworkImageWidget(
+            url: url,
+            fit: BoxFit.cover,
+            size: const Size(125, 125),
+          ),
+          const SizedBox(height: 4),
+          AppText(
+            radioName,
+            maxLines: 2,
+          )
+        ],
+      ),
+    );
+  }
+}
